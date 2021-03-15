b795a494edeb45de379341052b726338c03e2895
Brendan Collins <brendan.m.collins@outlook.com>
2021-03-15 06:35:38 +0000

    add tests for api.py

---
+++ test/test_api.py
@@ -0,0 +1,18 @@
+""" Test APIClient """
+import unittest
+from understatapi import APIClient
+
+
+class TestAPIClientDunder(unittest.TestCase):
+    """ Tests for all `__*__()` methods of `BaseEndpoint()` """
+
+    def setUp(self):
+        """ setUp() """
+        self.understat = APIClient()
+
+    def test_init(self):
+        """ test `__init__()` """
+        with self.subTest(test="league_endpoint"):
+            self.assertEqual(repr(self.understat.league), "<LeagueEndpoint>")
+        with self.subTest(test="player_endpoint"):
+            self.assertEqual(repr(self.understat.player), "<PlayerEndpoint>")
