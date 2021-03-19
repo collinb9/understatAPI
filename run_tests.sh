957e4661233560726e981704b0349ccfbfea1d98
Brendan Collins <brendan.m.collins@outlook.com>
2021-03-19 07:23:47 +0000

    releases

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,8 @@
 export CHANGED_FILES=`git diff --name-only --diff-filter=d origin/master | grep -E '\.py$' | tr '\n' ' '`
+if [ -z "$CHANGED_FILES" ]
+then
+		export CHANGED_FILES="understatapi/api.py"
+fi
 black --check --line-length=79 ${CHANGED_FILES}
 pylint ${CHANGED_FILES}
 mypy -p understatapi
