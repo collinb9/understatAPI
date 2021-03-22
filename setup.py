c52818ac46c169ef8917682ac450fbe8f42560ce
Brendan Collins <brendan.m.collins@outlook.com>
2021-03-22 06:28:31 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="understatapi",
-    version="0.1.0",
+    version="0.2.0",
     description="An API for scraping data from understat.com",
     packages=find_packages(exclude=("tests",)),
     long_description=long_description,
