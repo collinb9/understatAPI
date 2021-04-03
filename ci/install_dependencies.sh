434fc0dc2f5db2a5bfb5b9f9d638bb6895fa730a
Brendan <brendan.m.collins@outlook.com>
2021-04-03 15:13:59 +0100

    travis

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -4,5 +4,6 @@ if [ -f "/usr/local/bin/geckodriver" ]; then
     chmod +x geckodriver
     sudo mv geckodriver /usr/local/bin/
 fi
-pip install -r requirements.txt -r test_requirements.txt -r docs/requirements.txt
+pip install -r requirements.txt -r test_requirements.txt 
+pip install -r docs/requirements.txt
 
