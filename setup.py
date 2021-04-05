bb651623cf4bc0eb5acca463a50e337e7413f0f3
Brendan <brendan.m.collins@outlook.com>
2021-04-05 06:40:59 +0100

    setup change

--- setup.py
+++ setup.py
@@ -1,19 +1,36 @@
 """ setup for understatAPI """
+import os
+import re
 from setuptools import setup, find_packages
-from read_version import read_version
 
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+def read_version():
+    """ read the current version from understatapi.__init__.py """
+    with open(
+        os.path.join(os.path.dirname(__file__), "understatapi", "__init__.py")
+    ) as fpath:
+        for line in fpath:
+            match = re.search(
+                r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line
+            )
+            if match:
+                return match.group(2)
+    raise RuntimeError("Unable to find version")
 
-VERSION = read_version("understatapi", "__init__.py")
+
+def long_description():
+    """ Get description from readme """
+    with open("README.md", "r") as fpath:
+        long_des = fpath.read()
+    return long_des
+
 
 setup(
     name="understatapi",
-    version=VERSION,
+    version=read_version(),
     description="An API for scraping data from understat.com",
     packages=find_packages(exclude=("tests",)),
-    long_description=long_description,
+    long_description=long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/collinb9/understatAPI",
     author="collinb9",
@@ -22,7 +39,8 @@ setup(
     install_requires=["pandas>=1.1.0", "requests>=2.0.0", "selenium>=3.141.0"],
     keywords=(
         "statistics xG expected goals fpl fantasy"
-        "premier league understat football"
+        "premier league understat football web scraping"
+        "scraper"
     ),
     project_urls={
         "Source": "https://github.com/collinb9/understatAPI/",
