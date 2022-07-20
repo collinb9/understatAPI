0da3ac9796b8583b24c8ddca04e8dfeb83c26ed6
collinb9 <brendan.m.collins@outlook.com>
2022-07-20 20:18:39 +0000

    temp commit

--- cd/deploy_docs.sh
+++ cd/deploy_docs.sh
@@ -2,5 +2,5 @@
 mkdir /tmp/gh-pages
 cp -r docs/build/html /tmp/gh_pages
 git checkout gh-pages
-# rm -rf * && cp -r /tmp/gh-pages/* ./ && rm -rf /tmp/gh-pages && git add . && git commit -m "Updated gh-pages" && git push && git checkout master
+rm -rf * && cp -r /tmp/gh-pages/* ./ && rm -rf /tmp/gh-pages && git add . && git commit -m "Updated gh-pages" && git push && git checkout -
 
