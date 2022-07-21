fbcf22ccef9889ae4babcc86870345dee4f5ea89
collinb9 <brendan.m.collins@outlook.com>
2022-07-21 20:56:46 +0000

    Fix deploy job

--- cd/deploy_docs.sh
+++ cd/deploy_docs.sh
@@ -5,5 +5,10 @@ set -e
 mkdir -p /tmp/gh-pages
 cp -r docs/build/html /tmp/gh-pages
 git checkout gh-pages
-rm -rf * && cp -r /tmp/gh-pages/html/* ./ && rm -rf /tmp/gh-pages && git add . && git commit -m "Updated gh-pages" && git push && git checkout -
+rm -rf * && \
+    cp -r /tmp/gh-pages/html/* ./ && \
+    rm -rf /tmp/gh-pages && git add . && \
+    git commit -m "Updated gh-pages" && \
+    git push -q https://${GITHUB__TOKEN}@github.com/collinb9/understatapi.git gh-pages && \
+    git checkout -
 
