2d8f57f0b51b1bbdc5d406976d01bffae9107f9f
collinb9 <brendan.m.collins@outlook.com>
2022-07-22 13:12:34 +0200

    Debug

--- cd/deploy_docs.sh
+++ cd/deploy_docs.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
 set -e
+set -x
 
 mkdir -p /tmp/gh-pages
 cp -r docs/build/html /tmp/gh-pages
-git checkout gh-pages
+git checkout -b gh-pages
 rm -rf * && \
     cp -r /tmp/gh-pages/html/* ./ && \
     rm -rf /tmp/gh-pages && git add . && \
     git commit -m "Updated gh-pages" && \
-    git push \
+    git push --set-upstream origin gh-pages \
     git checkout -
 
