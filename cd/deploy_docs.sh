550dd341d692581da3abd68bf21e57475029d410
collinb9 <brendan.m.collins@outlook.com>
2022-07-22 13:47:40 +0200

    Prevent circleci from running a workflow for gh-pages branch

--- cd/deploy_docs.sh
+++ cd/deploy_docs.sh
@@ -2,12 +2,27 @@
 
 set -e
 
+generate_circleci_config()
+{
+  cat <<EOF
+workflows:
+  build-workflow:
+    jobs:
+      - build:
+          filters:
+            branches:
+              ignore: /.*/
+EOF
+}
+
 mkdir -p /tmp/gh-pages
 cp -r docs/build/html /tmp/gh-pages
 git checkout gh-pages
 git pull
 rm -rf * && \
     cp -r /tmp/gh-pages/html/* ./ && \
+    mkdir -p .circleci \\
+    generate_circleci_config > .circleci/config.yml \\
     rm -rf /tmp/gh-pages && git add . && \
     git commit -m "Updated gh-pages" && \
     git push && \
