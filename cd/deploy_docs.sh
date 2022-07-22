8dd384c5d56ceea1ce59592ad50bc46c4ce154cb
collinb9 <brendan.m.collins@outlook.com>
2022-07-22 13:15:54 +0200

    Fix deploy_docs script

--- cd/deploy_docs.sh
+++ cd/deploy_docs.sh
@@ -10,6 +10,6 @@ rm -rf * && \
     cp -r /tmp/gh-pages/html/* ./ && \
     rm -rf /tmp/gh-pages && git add . && \
     git commit -m "Updated gh-pages" && \
-    git push --set-upstream origin gh-pages \
+    git push --set-upstream origin gh-pages && \
     git checkout -
 
